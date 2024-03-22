a5dfde1d79d4b70b30a590810fe26d8c00833729
9175616056 <rohanawale73@gmail,com>
2024-03-22 17:09:25 +0530

    first commit

---
+++ src/main/java/com/blogapp111/Blogapp1204Application.java
@@ -0,0 +1,13 @@
+package com.blogapp111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogapp1204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blogapp1204Application.class, args);
+	}
+
+}
